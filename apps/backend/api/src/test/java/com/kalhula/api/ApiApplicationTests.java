26755790ce765e2c319a83ca675ff369554a809c
CustCoding <custodiotitossetitosse@gmail.com>
2026-07-01 14:29:26 +0200

    chore(api): initialize backend structure

---
+++ apps/backend/api/src/test/java/com/kalhula/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalhula.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
